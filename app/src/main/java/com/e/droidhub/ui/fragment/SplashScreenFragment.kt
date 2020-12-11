967a5ac91ef74f888d8f9b34c9bb7f68fc37adc6
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2020-12-11 21:41:56 +0100

    include SplashScreenFragment

--- app/src/main/java/com/e/droidhub/ui/fragment/SplashScreenFragment.kt
+++ app/src/main/java/com/e/droidhub/ui/fragment/SplashScreenFragment.kt
@@ -6,24 +6,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.e.droidhub.R
 import com.e.droidhub.databinding.FragmentSignupBinding
 
 
 class SplashScreenFragment : Fragment() {
-    private var _binding: FragmentSignupBinding? = null
-    private val binding get() = _binding!!
-    private val handler = Handler()
-    private val runnable = Runnable {
-
-    }
+    private val SPLASH_TIME_OUT = 3000L
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return binding?.root
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed(
+                {
+                    findNavController().navigate(R.id.loginFragment)
+                }, SPLASH_TIME_OUT)
     }
 
 }
